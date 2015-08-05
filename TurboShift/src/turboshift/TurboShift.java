1e4147627bb73730111793fafac2adc7cf476945
Guilherme Paes <guilhermep98@gmail.com>
2015-08-05 17:15:35 -0300

    Tela Splash adicionada

--- TurboShift/src/turboshift/TurboShift.java
+++ TurboShift/src/turboshift/TurboShift.java
@@ -1,22 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package turboshift;
 
-/**
- *
- * @author Iserj
- */
-public class TurboShift {
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class TurboShift  {
+   
+
+
+
+
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+           TelaSplash  t = new TelaSplash(10000);
+            t.showSplashAndExit();        
     }
     
 }
